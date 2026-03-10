9d6c4cb75263af4e538063498f0dabd402875069
jtb75 <joe.buhr@gmail.com>
2026-03-10 10:44:10 -0400

    Fix state cleanup and prevent EKS cluster replacement
    - Add removed blocks to drop kubernetes resources from state without
      destroying them (namespace/SA managed by Outpost Lite workflow)
    - Re-add kubernetes provider temporarily for state transition
    - Set bootstrap_cluster_creator_admin_permissions to prevent
      force-replacement of the EKS cluster

--- remediation-infra/eks.tf
+++ remediation-infra/eks.tf
@@ -66,7 +66,8 @@ resource "aws_eks_cluster" "remediation" {
   }
 
   access_config {
-    authentication_mode = "API_AND_CONFIG_MAP"
+    authentication_mode                         = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
   }
 
   depends_on = [
