12b0a64fd2b4bbb256562c088a75af087b1202b1
jtb75 <joe.buhr@gmail.com>
2026-03-10 11:38:38 -0400

    Trim to single remediation runner: rem-aws-ebs-007
    Start with just EBS snapshot attribute modification while validating
    the end-to-end remediation flow in the portal.

--- remediation-infra/iam_remediation.tf
+++ remediation-infra/iam_remediation.tf
@@ -15,61 +15,6 @@ module "remediation_roles" {
       "ec2:ModifySnapshotAttribute",
       "ec2:DescribeSnapshotAttribute",
       "ec2:CreateTags"
-    ],
-    "rem-aws-ec2-031" : [
-      "tag:TagResources",
-      "ec2:DescribeSecurityGroups",
-      "ec2:RevokeSecurityGroupIngress",
-      "ec2:CreateTags"
-    ],
-    "rem-aws-iam-025" : [
-      "tag:TagResources",
-      "iam:CreatePolicyVersion",
-      "iam:TagUser",
-      "iam:TagPolicy",
-      "iam:PutRolePolicy",
-      "iam:PutGroupPolicy",
-      "iam:PutUserPolicy",
-      "iam:TagRole"
-    ],
-    "rem-aws-iam-045" : [
-      "tag:TagResources",
-      "iam:GetAccessKeyLastUsed",
-      "iam:UpdateAccessKey",
-      "iam:ListAccessKeys"
-    ],
-    "rem-aws-iam-223" : [
-      "iam:TagRole",
-      "iam:GetRole",
-      "iam:ListAttachedRolePolicies",
-      "iam:ListRolePolicies",
-      "iam:ListInstanceProfilesForRole",
-      "iam:DetachRolePolicy",
-      "iam:DeleteRolePolicy",
-      "iam:RemoveRoleFromInstanceProfile",
-      "iam:DeleteRole"
-    ],
-    "rem-aws-rds-002" : [
-      "tag:TagResources",
-      "rds:AddTagsToResource",
-      "rds:DescribeDBInstances",
-      "rds:ModifyDBInstance"
-    ],
-    "rem-aws-rds-003" : [
-      "tag:TagResources",
-      "rds:ModifyDBSnapshotAttribute",
-      "rds:AddTagsToResource",
-      "rds:DescribeDBSnapshotAttributes"
-    ],
-    "rem-aws-s3-046" : [
-      "tag:TagResources",
-      "s3:PutBucketTagging",
-      "s3:PutBucketPolicy",
-      "s3:PutBucketPublicAccessBlock",
-      "s3:DeleteBucketPolicy",
-      "s3:GetBucketPolicy",
-      "s3:GetBucketTagging",
-      "s3:GetBucketPublicAccessBlock"
     ]
   }
 }
