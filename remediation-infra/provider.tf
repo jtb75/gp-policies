804c629a37e9148e8c4c9d2c803c2a69326d028b
jtb75 <joe.buhr@gmail.com>
2026-03-10 10:40:46 -0400

    Remove kubernetes/helm providers from remediation-infra
    The kubernetes and helm providers fail during plan when the EKS cluster
    endpoint is not yet known, causing localhost connection errors. Namespace
    and service account creation is handled by the Outpost Lite Helm workflow
    instead.

--- remediation-infra/provider.tf
+++ remediation-infra/provider.tf
@@ -4,14 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
-    }
   }
 
   backend "s3" {
@@ -29,28 +21,6 @@ provider "aws" {
     tags = {
       ManagedBy = "terraform"
       Project   = "gp-policies-remediation"
-    }
-  }
-}
-
-provider "kubernetes" {
-  host                   = aws_eks_cluster.remediation.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.remediation.certificate_authority[0].data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.remediation.name]
-  }
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = aws_eks_cluster.remediation.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.remediation.certificate_authority[0].data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.remediation.name]
     }
   }
 }
