55ed4c47c9d43f59cd91bf71f4efcf92b25405b8
jtb75 <joe.buhr@gmail.com>
2026-03-10 21:50:23 -0400

    Update IAM and Helm values for new R&R deployment
    Replace old runner IDs (rem-aws-ebs-007, rem-custom-0008) with
    new deployment-generated IDs (rem-custom-0009/0010/0011) for
    untrusted trust removal, consumer role tagging, and stale key
    deactivation actions.

--- remediation-infra/iam_remediation.tf
+++ remediation-infra/iam_remediation.tf
@@ -10,15 +10,21 @@ module "remediation_roles" {
   resources_prefix = "Wiz"
 
   permission_sets = {
-    "rem-aws-ebs-007" : [
-      "tag:TagResources",
-      "ec2:ModifySnapshotAttribute",
-      "ec2:DescribeSnapshotAttribute",
-      "ec2:CreateTags"
+    "rem-custom-0009" : [
+      "iam:GetRole",
+      "iam:TagRole",
+      "iam:UpdateAssumeRolePolicy"
     ],
-    "rem-custom-0008" : [
+    "rem-custom-0010" : [
       "iam:GetRole",
       "iam:TagRole"
+    ],
+    "rem-custom-0011" : [
+      "iam:GetAccessKeyLastUsed",
+      "iam:ListAccessKeys",
+      "iam:ListUserTags",
+      "iam:TagUser",
+      "iam:UpdateAccessKey"
     ]
   }
 }
